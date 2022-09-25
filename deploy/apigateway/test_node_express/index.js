060e8ae701b249cef2cd0f052140d9839a61f6f0
minh.tranvan <minh.tranvan@hdwebsoft.io>
2022-09-25 21:26:59 +0700

    add haproxy and kongapi

---
+++ deploy/apigateway/test_node_express/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express()
+const port = 3000
+server.get('/', (req, res) => {
+  console.log(req.headers)
+  res.status(200).send('Hello world!')
+})
+server.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`)
+})

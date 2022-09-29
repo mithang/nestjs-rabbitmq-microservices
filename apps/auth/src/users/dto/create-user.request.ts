1ab17b99e00aba4df768893d9a413ba4ddeb31c2
minh.tranvan <minh.tranvan@hdwebsoft.io>
2022-09-29 23:47:01 +0700

    add description field in swagger

--- apps/auth/src/users/dto/create-user.request.ts
+++ apps/auth/src/users/dto/create-user.request.ts
@@ -1,9 +1,17 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserRequest {
+  @ApiProperty({
+    example: 'test@yahoo.com',
+    description: 'The email of the customer',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'password 123',
+    description: 'The password of the customer',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
--- apps/orders/src/dto/create-order.request.ts
+++ apps/orders/src/dto/create-order.request.ts
@@ -4,15 +4,25 @@ import {
   IsPositive,
   IsString,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderRequest {
+  @ApiProperty({
+    example: 'product a',
+    description: 'The name of the customer',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ example: 10000, description: 'The price of the customer' })
   @IsPositive()
   price: number;
 
+  @ApiProperty({
+    example: '0934242422',
+    description: 'The phone of the customer',
+  })
   // @IsPhoneNumber('US')
   phoneNumber: string;
 }
